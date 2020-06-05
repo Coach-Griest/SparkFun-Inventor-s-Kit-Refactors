46118697cba311d2cd023668a010767e8d5c3657
mitchellgriest <mitchellgriest@westminster.net>
2020-06-05 13:39:46 -0400

    Added comments to top of file for Boolean NOT circuit code

--- My Own Circuits/Boolean_NOT/Boolean_NOT.ino
+++ My Own Circuits/Boolean_NOT/Boolean_NOT.ino
@@ -1,3 +1,10 @@
+/*
+ * Boolean NOT circuit using a button and an LED
+ * by Mitchell Griest, Summer 2020
+ * 
+ * TinkerCad Circuit link: https://www.tinkercad.com/things/a5petd4HbrE-mighty-luulia-esboo/editel?tenant=circuits?sharecode=Mismn8_1YfFj8bcDSD2uctGl2eyOH_kL8kJZgohA7eM
+ */
+
 const int ledPin = 13;
 const int buttonPin = 8;
 
